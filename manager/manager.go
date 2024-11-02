4fa660c99f1ef8c5271c7fa696fbd4a2d48a134f
gui <guilhermev2huehue@gmail.com>
2024-11-02 14:08:49 -0300

    feat: manager skeleton

---
+++ manager/manager.go
@@ -0,0 +1,24 @@
+package manager
+
+import (
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"github.com/haveheartt/orchestrator-from-scratch/task"
+)
+
+type Manager struct {
+    Pending         queue.Queue
+    TaskDb          map[string][]*task.Task
+    EventDb         map[string][]*task.TaskEvent
+    Workers         []string
+    WorkerTaskMap   map[string]uuid.UUID
+    TaskWorkerMap   map[uuid.UUID]string
+}
+
+func (m *Manager) SelectWorker() {}
+
+func (m *Manager) UpdateTasks() {}
+
+func (m *Manager) SendWork() {}
+
+
